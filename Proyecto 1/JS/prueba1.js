09aa9289fba9c5f082f7f7776fdda8f9a0c56209
Juan Baldonedo <50972039+Gonpers@users.noreply.github.com>
2023-07-18 00:15:30 -0400

    prueba v1.2
    -Tipos de variables
    -alerts vinculadas a botones
    -objetos
    -json
    -prompt

--- Proyecto 1/JS/prueba1.js
+++ Proyecto 1/JS/prueba1.js
@@ -4,4 +4,51 @@ function suma(){
     return "hola soy una funcion"
 }
  
-console.log(suma())
+
+let auto = {
+    modelo:"neon",
+    color: "gris",
+    
+}
+
+let listaAutos =[
+{modelo:"mazda",
+color:"negro"
+},
+{modelo:"malibu",
+color:"blanco"
+}
+
+]
+
+console.log(listaAutos);
+
+console.log(auto);
+
+let realName;
+
+let userName = 'Rigbebito';
+
+var userName2 = 'digrat';
+
+const pi = 3.14159265359 ;
+
+console.log(userName)
+
+//realName = prompt("Quien sos vos??");
+//formularios
+//username
+//password
+//fecha de nacimiento
+
+//y enviar los datos por consola
+
+function showAlert (){
+
+    alert(`Felicidades ${realName}, ahora eres un ${userName} wooooooo.`);
+}
+
+function showAlert2 (){
+
+    alert (`Oh no ${realName}, te transformaste en un ${userName2} gg no team`);
+}
